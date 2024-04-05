9fdceff7954eecc1ca80856e49eff8bffe37a3d4
calt-laboratory <christoph_alt@posteo.de>
2024-04-05 15:46:20 +0200

    feat(dynamic_array): added shrinking to pop() + delete_at()

--- src/dynamic_array.py
+++ src/dynamic_array.py
@@ -1,6 +1,6 @@
 class DynamicArray:
     def __init__(self) -> None:
-        self._n_items = 0  # current item number in the array
+        self._n_items = 0  # current number of items in the array
         self._array_capacity = 1
         self._array = self._create_array(array_capacity=self._array_capacity)
 
@@ -14,13 +14,13 @@ class DynamicArray:
             raise IndexError(f"Index {idx} does not lie inside the array size of {self._n_items}")
         return self._array[idx]
 
-    def append(self, new_element: int) -> list:
+    def append(self, new_item: int) -> list:
         """Appends a new element at the end of the array."""
         if self._n_items == self._array_capacity:
             new_capacity = 2 * self._array_capacity
             self._resize_array(new_capacity=new_capacity)
 
-        self._array[self._n_items] = new_element
+        self._array[self._n_items] = new_item
         self._n_items += 1
         print(f"Array: {self._array}")
 
@@ -33,7 +33,10 @@ class DynamicArray:
         self._n_items -= 1
         print(f"Array: {self._array}")
 
-    def insert_at(self, idx: int, new_element: int) -> None:
+        if self._n_items == self._array_capacity // 2:
+            self._resize_array(new_capacity=self._array_capacity // 2)
+
+    def insert_at(self, idx: int, new_item: int) -> None:
         """Inserts a new element at a given index and moves the subsequent elements to the right."""
         if not 0 <= idx < self._n_items:
             raise IndexError(f"Index {idx} does not lie inside the array size of {self._n_items}")
@@ -45,7 +48,7 @@ class DynamicArray:
         for i in range(self._n_items - 1, idx - 1, -1):  # Transversing backwards through the array
             self._array[i + 1] = self._array[i]
 
-        self._array[idx] = new_element
+        self._array[idx] = new_item
         self._n_items += 1
         print(f"Array: {self._array}")
         
@@ -57,9 +60,11 @@ class DynamicArray:
         self._array[idx] = None
         for i in range(idx + 1, self._n_items + 1):
             self._array[i - 1] = self._array[i]
-
-        self._n_items -= 1
         print(f"Array: {self._array}")
+        self._n_items -= 1
+
+        if self._n_items == self._array_capacity // 2:
+            self._resize_array(new_capacity=self._array_capacity // 2)
 
     @staticmethod
     def _create_array(array_capacity: int) -> list:
@@ -75,20 +80,23 @@ class DynamicArray:
 
         self._array = new_array
         self._array_capacity = new_capacity
-        print(f"Array capacity: {self._array_capacity}")
+        print(f"Array: {self._array}")
 
 
 def main() -> None:
     dynamic_array = DynamicArray()
-    dynamic_array.append(new_element=32)
-    dynamic_array.append(new_element=11)
-    dynamic_array.append(new_element=22)
-    dynamic_array.append(new_element=77)
-    dynamic_array.insert_at(idx=2, new_element=42)
+    dynamic_array.append(new_item=32)
+    dynamic_array.append(new_item=11)
+    dynamic_array.append(new_item=22)
+    dynamic_array.append(new_item=77)
+    dynamic_array.insert_at(idx=2, new_item=42)
     dynamic_array.pop()
-    dynamic_array.append(new_element=100)
-    dynamic_array.append(new_element=55)
+    dynamic_array.append(new_item=100)
+    dynamic_array.append(new_item=55)
     dynamic_array.delete_at(idx=3)
+    dynamic_array.delete_at(idx=0)
+    dynamic_array.insert_at(idx=1, new_item=227)
+    dynamic_array.pop()
 
 
 if __name__ == "__main__":
