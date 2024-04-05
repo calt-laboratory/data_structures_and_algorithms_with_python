ca4dae5b314eee419fe4123da7235c6ec34bb74d
calt-laboratory <christoph_alt@posteo.de>
2024-04-05 14:48:55 +0200

    refactor(dynamic_array): renamed vars + change Any to int

--- src/dynamic_array.py
+++ src/dynamic_array.py
@@ -1,57 +1,57 @@
-from typing import Any
-
-
 class DynamicArray:
     def __init__(self) -> None:
-        self._current_number_of_elements = 0
+        self._n_items = 0  # current item number in the array
         self._array_capacity = 1
         self._array = self._create_array(array_capacity=self._array_capacity)
 
     def __len__(self) -> int:
         """Returns length of the array."""
-        return self._current_number_of_elements
+        return self._n_items
 
-    def __getitem__(self, idx: int) -> Any:
+    def __getitem__(self, idx: int) -> int:
         """Returns element at a given index."""
-        if not 0 <= idx < self._current_number_of_elements:
-            raise IndexError(f"Index {idx} does not lie inside the array size of {self._current_number_of_elements}")
+        if not 0 <= idx < self._n_items:
+            raise IndexError(f"Index {idx} does not lie inside the array size of {self._n_items}")
         return self._array[idx]
 
-    def append(self, new_element: Any) -> list:
+    def append(self, new_element: int) -> list:
         """Appends a new element at the end of the array."""
-        if self._current_number_of_elements == self._array_capacity:
+        if self._n_items == self._array_capacity:
             new_capacity = 2 * self._array_capacity
             self._resize_array(new_capacity=new_capacity)
 
-        self._array[self._current_number_of_elements] = new_element
-        self._current_number_of_elements += 1
+        self._array[self._n_items] = new_element
+        self._n_items += 1
         print(f"Array: {self._array}")
 
     def pop(self) -> None:
         """Pops the last element of the array."""
-        if self._current_number_of_elements == 0:
+        if self._n_items == 0:
             raise Exception("The array is empty. Pop is not possible.")
 
-        self._array[self._current_number_of_elements - 1] = None
-        self._current_number_of_elements -= 1
+        self._array[self._n_items - 1] = None
+        self._n_items -= 1
         print(f"Array: {self._array}")
 
-    def insert_at(self, idx: int, new_element: Any) -> None:
+    def insert_at(self, idx: int, new_element: int) -> None:
         """Inserts a new element at a given index and moves the subsequent elements to the right."""
-        if not 0 <= idx < self._current_number_of_elements:
-            raise IndexError(f"Index {idx} does not lie inside the array size of {self._current_number_of_elements}")
+        if not 0 <= idx < self._n_items:
+            raise IndexError(f"Index {idx} does not lie inside the array size of {self._n_items}")
 
-        if self._current_number_of_elements == self._array_capacity:
+        if self._n_items == self._array_capacity:
             new_capacity = 2 * self._array_capacity
             self._resize_array(new_capacity=new_capacity)
 
-        # Transversing backwards through the array
-        for i in range(self._current_number_of_elements - 1, idx - 1, -1):
+        for i in range(self._n_items - 1, idx - 1, -1):  # Transversing backwards through the array
             self._array[i + 1] = self._array[i]
 
         self._array[idx] = new_element
-        self._current_number_of_elements += 1
+        self._n_items += 1
         print(f"Array: {self._array}")
+        
+    def delete_at(self, idx: int) -> None:
+        """Deletes an item at a given index and move the subsequent items to the left."""
+        ...
 
     @staticmethod
     def _create_array(array_capacity: int) -> list:
@@ -62,7 +62,7 @@ class DynamicArray:
         """Resizes the array to a given capacity."""
         new_array = self._create_array(array_capacity=new_capacity)
 
-        for i in range(self._current_number_of_elements):
+        for i in range(self._n_items):
             new_array[i] = self._array[i]
 
         self._array = new_array
