016da2d57d16b8a93b49bec40d83733edddcf40c
calt-laboratory <christoph_alt@posteo.de>
2024-04-05 11:46:14 +0200

    feat(dynamic_array): implemented insert_at()

--- src/dynamic_array.py
+++ src/dynamic_array.py
@@ -25,14 +25,33 @@ class DynamicArray:
 
         self._array[self._current_number_of_elements] = new_element
         self._current_number_of_elements += 1
+        print(f"Array: {self._array}")
 
     def pop(self) -> None:
         """Pops the last element of the array."""
         if self._current_number_of_elements == 0:
-            print("The array is empty.")
+            raise Exception("The array is empty. Pop is not possible.")
 
         self._array[self._current_number_of_elements - 1] = None
         self._current_number_of_elements -= 1
+        print(f"Array: {self._array}")
+
+    def insert_at(self, idx: int, new_element: Any) -> None:
+        """Inserts a new element at a given index and moves the subsequent elements to the right."""
+        if not 0 <= idx < self._current_number_of_elements:
+            raise IndexError(f"Index {idx} does not lie inside the array size of {self._current_number_of_elements}")
+
+        if self._current_number_of_elements == self._array_capacity:
+            new_capacity = 2 * self._array_capacity
+            self._resize_array(new_capacity=new_capacity)
+
+        # Transversing backwards through the array
+        for i in range(self._current_number_of_elements - 1, idx - 1, -1):
+            self._array[i + 1] = self._array[i]
+
+        self._array[idx] = new_element
+        self._current_number_of_elements += 1
+        print(f"Array: {self._array}")
 
     @staticmethod
     def _create_array(array_capacity: int) -> list:
@@ -48,18 +67,17 @@ class DynamicArray:
 
         self._array = new_array
         self._array_capacity = new_capacity
+        print(f"Array capacity: {self._array_capacity}")
 
 
 def main() -> None:
     dynamic_array = DynamicArray()
-    dynamic_array.append(new_element=100)
-    dynamic_array.append(new_element=24)
-    print(dynamic_array[0])
-    print(dynamic_array[1])
-    print(len(dynamic_array))
+    dynamic_array.append(new_element=32)
+    dynamic_array.append(new_element=11)
+    dynamic_array.append(new_element=22)
+    dynamic_array.append(new_element=77)
+    dynamic_array.insert_at(idx=2, new_element=111)
     dynamic_array.pop()
-    print(len(dynamic_array))
-    print(dynamic_array[1])
 
 
 if __name__ == "__main__":
