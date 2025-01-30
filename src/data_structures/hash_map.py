5d9aeb7dfa5b4a4c698bb4726910188785ad1ebe
calt-lab <christoph_alt@posteo.de>
2025-01-31 00:22:19 +0100

    feat(hash map): implemented get method + optimized hash map structure

--- src/data_structures/hash_map.py
+++ src/data_structures/hash_map.py
@@ -4,30 +4,40 @@ from typing import Optional
 class HashMap:
     def __init__(self, size: int) -> None:
         self.size = size
-        self.hash_map: list[Optional[int]] = [None] * self.size
+        self.hash_map = [(None, ) for _ in range(self.size)]
 
 
     def _hash_function(self, key: int) -> int:
         return key % self.size
 
-    def rehash_by_open_addressing(self, old_hash: int) -> int:
-        return (old_hash + 1) % self.size
-
     def put(self, key: int, value: int | float | str) -> None:
         # Convert/Compute a hashed key (index) from the key
         idx = self._hash_function(key=key)
 
-        if self.hash_map[idx] is None:
+        if self.hash_map[idx][0] is None:
             self.hash_map[idx] = (key, value)
             return None
 
         if self.hash_map[idx][0] != key:
-            for i, pair in enumerate(self.hash_map):
-                if pair is None:
+            for i, pair in enumerate(self.hash_map[idx + 1:], start=idx + 1):
+                if pair[0] is None:
                     self.hash_map[i] = (key, value)
                     return None
 
         return None
+
+    def get(self, key: int) -> Optional[int | float | str]:
+
+        idx = self._hash_function(key=key)
+
+        if key == self.hash_map[idx][0]:
+            return self.hash_map[idx][1]
+        else:
+            for pair in self.hash_map[idx + 1:]:
+                if key == pair[0]:
+                    return pair[1]
+
+        raise KeyError
 
     def __str__(self) -> str:
         return str([item for item in self.hash_map])
@@ -48,6 +58,11 @@ def main() -> None:
     print(hash_map)
     hash_map.put(key=22, value="Twenty Two")
     print(hash_map)
+    print(hash_map.get(key=22))
+    print(hash_map.get(key=11))
+    print(hash_map.get(key=24))
+    print(hash_map.get(key=42))
+    print(hash_map.get(key=11111))
 
 
 if __name__ == "__main__":
