d049dd744a4ae08ba7abfcbbbe528472f71dac5e
calt-lab <christoph_alt@posteo.de>
2025-01-30 23:12:51 +0100

    feat(hash map): optimized hash map structure

--- src/data_structures/hash_map.py
+++ src/data_structures/hash_map.py
@@ -4,8 +4,7 @@ from typing import Optional
 class HashMap:
     def __init__(self, size: int) -> None:
         self.size = size
-        self.hash_map_keys: list[Optional[int]] = [None] * self.size
-        self.hash_map_values: list[Optional[int]] = [None] * self.size
+        self.hash_map: list[Optional[int]] = [None] * self.size
 
 
     def _hash_function(self, key: int) -> int:
@@ -14,27 +13,41 @@ class HashMap:
     def rehash_by_open_addressing(self, old_hash: int) -> int:
         return (old_hash + 1) % self.size
 
-    def put(self, key: int, value: int) -> None:
+    def put(self, key: int, value: int | float | str) -> None:
         # Convert/Compute a hashed key (index) from the key
         idx = self._hash_function(key=key)
 
-        if self.hash_map_keys[idx] is None:
-            self.hash_map_keys[idx] = key
-            self.hash_map_values[idx] = value
-            return
+        if self.hash_map[idx] is None:
+            self.hash_map[idx] = (key, value)
+            return None
 
-        if self.hash_map_keys[idx] == key:
-            pass
+        if self.hash_map[idx][0] != key:
+            for i, pair in enumerate(self.hash_map):
+                if pair is None:
+                    self.hash_map[i] = (key, value)
+                    return None
 
+        return None
+
+    def __str__(self) -> str:
+        return str([item for item in self.hash_map])
 
     def __len__(self) -> int:
-        return len(self.hash_map_keys)
+        return len(self.hash_map)
 
 
 def main() -> None:
-    hm = HashMap(size=10)
-    print(hm)
-    print(len(hm))
+    hash_map = HashMap(size=10)
+    print(hash_map)
+    # print(len(hash_map))
+    hash_map.put(key=42, value="Douglas Adams")
+    print(hash_map)
+    hash_map.put(key=24, value="NOT")
+    print(hash_map)
+    hash_map.put(key=11, value="Eleven")
+    print(hash_map)
+    hash_map.put(key=22, value="Twenty Two")
+    print(hash_map)
 
 
 if __name__ == "__main__":
